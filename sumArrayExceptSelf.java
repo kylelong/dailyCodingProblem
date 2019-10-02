5390fd5597a6806cf01e5f73a6f4c6525a4e9cd1
Kyle Long <kylel95@vt.edu>
2019-10-02 15:06:09 -0400

    sumArrayExceptSelf done agin

--- sumArrayExceptSelf.java
+++ sumArrayExceptSelf.java
@@ -15,8 +15,18 @@ public class sumArrayExceptSelf{
     /**
     Returns array of that i is product of everythign but i itself
      */
-    public static int [] sumArray(int [] arr){
-
+    public static int [] sumArray(int [] a){
+      int [] b = new int[a.length];
+      b[0] = 1;
+      for(int i = 1; i < a.length; i++){
+          b[i] = b[i - 1] * a[i - 1];
+      }
+      int num = 1;
+      for(int i = a.length - 1; i>= 0; i--){
+          b[i] *= num;
+          num *= a[i];
+      }
+      return b;
        
     }
 
